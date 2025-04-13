63d02eb065e4e1e3b4003b37dd3adef06ef3a329
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-04-14 00:08:19 +0500

    set port

--- backend/index.mjs
+++ backend/index.mjs
@@ -33,5 +33,5 @@ app.use("/", (req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${5000}`);
 });
