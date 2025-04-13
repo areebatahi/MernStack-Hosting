646f18489bf3c2cb61a9de91dacdeeaa2f065b58
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2025-04-13 06:20:01 +0500

    frontend link add

--- backend/index.mjs
+++ backend/index.mjs
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5174', 'http://localhost:5173','https://mer-nstack-batch11-section-j-three.vercel.app'],
+		origin: ['http://localhost:5174', 'http://localhost:5173','https://mern-stack-hosting.vercel.app'],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
